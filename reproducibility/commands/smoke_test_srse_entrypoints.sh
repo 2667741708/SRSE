49713a2d3969d6bd5ba3ec63633f17dbf69e0124
2667741708 <131998384+2667741708@users.noreply.github.com>
2026-07-23 11:34:43 +0800

    Remove unused label-smoothing option

--- reproducibility/commands/smoke_test_srse_entrypoints.sh
+++ reproducibility/commands/smoke_test_srse_entrypoints.sh
@@ -84,7 +84,6 @@ common_cifar_args=(
   --momentum 0.9
   --lr_scheduler cosine
   --mixup_alpha 1.0
-  --lsr 0.0
   --ema_alpha 0.999
   --k_val 15
   --delta 0.25
@@ -155,7 +154,6 @@ run_crowd_main() {
     --lr_decay_epochs 60 80 \
     --lr_decay_rate 0.2 \
     --mixup_alpha 1.0 \
-    --lsr 0.0 \
     --ema_alpha 0.999 \
     --k_val 5 \
     --delta 1.0 \
